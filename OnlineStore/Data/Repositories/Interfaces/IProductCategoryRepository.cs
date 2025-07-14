1c64249c01053fe0e0951d6f98cd3d1d60b56c1e
Светлана Тумоякова <Sveta.Tumoyakova@yandex.ru>
2025-07-15 00:44:33 +0700

    Update ProductCategoryRepository

--- OnlineStore/Data/Repositories/Interfaces/IProductCategoryRepository.cs
+++ OnlineStore/Data/Repositories/Interfaces/IProductCategoryRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace OnlineStore.Data.Repositories.Interfaces
+﻿using OnlineStore.Model;
+
+namespace OnlineStore.Data.Repositories.Interfaces
 {
     public interface IProductCategoryRepository
     {
+        Task<List<ProductCategory>> GetAll();
+        Task<ProductCategory> GetById(int id);
+        Task<ProductCategory> GetByIdTrackingAsync(int id);
+        Task AddAsync(ProductCategory productCategory);
+        Task Update(ProductCategory updateProductCategory);
+        Task RemoveAsync(int id);
+        Task<List<ProductCategory>> GetRange(int skip, int take);
+
     }
 }
--- OnlineStore/Data/Repositories/ProductCategoryRepository.cs
+++ OnlineStore/Data/Repositories/ProductCategoryRepository.cs
@@ -1,17 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Data.Repositories.Interfaces;
 using OnlineStore.Model;
 using System.Threading.Tasks;
 
 namespace OnlineStore.Data.Repositories
 {
-    public class ProductCategoryRepository
+    public class ProductCategoryRepository : IProductCategoryRepository
     {
         private OnlineStoreDBContext _dbContext;
         public ProductCategoryRepository(OnlineStoreDBContext dbContext)
         {
             _dbContext = dbContext;
         }
-        private Task<List<ProductCategory>> GetAll()
+        public Task<List<ProductCategory>> GetAll()
         {
             var productCategories = _dbContext.ProductCategories
                                                      .AsNoTracking()
@@ -19,33 +20,30 @@ namespace OnlineStore.Data.Repositories
             return productCategories;
         }
 
-        private Task<ProductCategory> GetById(int id)
+        public Task<ProductCategory> GetById(int id)
         {
             var productCategories = _dbContext.ProductCategories
                              .AsNoTracking()
                              .FirstOrDefaultAsync(productCategories => productCategories.Id == id);
             return productCategories;
         }
-        private async Task<ProductCategory> GetByIdTrackingAsync(int id)
+        public async Task<ProductCategory> GetByIdTrackingAsync(int id)
         {
             var productCategory = await _dbContext.ProductCategories.FirstOrDefaultAsync(productCategory => productCategory.Id == id);
             return productCategory;
         }
-        private Task<List<ProductCategory>> GetRange(int skip, int take)
-        {
-            var productCategories = _dbContext.ProductCategories
-                                                        .AsNoTracking()
-                                                        .Skip(skip)
-                                                        .Take(take)
-                                                        .ToListAsync();
-            return productCategories;
-        }
+
         public async Task AddAsync(ProductCategory productCategory)
         {
             await _dbContext.AddAsync(productCategory);
             await _dbContext.SaveChangesAsync();
         }
-
+        public async Task Update(ProductCategory updateProductCategory)
+        {
+            var productCategory = await GetByIdTrackingAsync(updateProductCategory.Id);
+            _dbContext.Remove(productCategory);
+            await _dbContext.SaveChangesAsync();
+        }
         public async Task RemoveAsync(int id)
         {
             var productCategory = await GetByIdTrackingAsync(id);
@@ -53,11 +51,14 @@ namespace OnlineStore.Data.Repositories
             await _dbContext.SaveChangesAsync();
         }
 
-        public async Task Update(ProductCategory updateProductCategory)
+        public Task<List<ProductCategory>> GetRange(int skip, int take)
         {
-            var productCategory = await GetByIdTrackingAsync(updateProductCategory.Id);
-            _dbContext.Remove(productCategory);
-            await _dbContext.SaveChangesAsync();
+            var productCategories = _dbContext.ProductCategories
+                                                        .AsNoTracking()
+                                                        .Skip(skip)
+                                                        .Take(take)
+                                                        .ToListAsync();
+            return productCategories;
         }
     }
 }
