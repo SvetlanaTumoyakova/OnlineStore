6ac2199a561bff5ad88a6c8a88f72363d37a08dd
Светлана Тумоякова <Sveta.Tumoyakova@yandex.ru>
2025-07-15 02:34:16 +0700

    Update ProductCategory Controller

--- OnlineStore/Controllers/ProductCategoryController.cs
+++ OnlineStore/Controllers/ProductCategoryController.cs
@@ -1,20 +1,78 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OnlineStore.Data;
+using OnlineStore.Data.Repositories.Interfaces;
+using OnlineStore.Exeptions;
+using OnlineStore.Model;
 
 namespace OnlineStore.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class ProductCategoryController : Controller
     {
-        private OnlineStoreDBContext _dbContext;
-        public ProductCategoryController(OnlineStoreDBContext dbContext)
+        private IProductCategoryRepository _productCategoryRepository;
+        public ProductCategoryController(IProductCategoryRepository productCategoryRepository)
         {
-            _dbContext = dbContext;
+            _productCategoryRepository = productCategoryRepository;
         }
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<ActionResult> GetAllAsync()
         {
-            return View();
+            var productCategories = await _productCategoryRepository.GetAllAsync();
+
+            return Ok(productCategories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetByIdAsync(int id)
+        {
+            var productCategory = await _productCategoryRepository.GetByIdAsync(id);
+
+            return Ok(productCategory);
+        }
+
+        [HttpGet("{skip}/{take}")]
+        public async Task<ActionResult> GetRangeAsync(int skip, int take)
+        {
+            var productCategories = await _productCategoryRepository.GetRangeAsync(skip, take);
+
+            return Ok(productCategories);
+        }
+        [HttpPost]
+        public async Task<ActionResult> AddAsync(ProductCategory product)
+        {
+           await _productCategoryRepository.AddAsync(product);
+           return Created();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateAsync(ProductCategory productCategory)
+        {
+            try
+            {
+                await _productCategoryRepository.UpdateAsync(productCategory);
+            }
+            catch (NotFoundException)
+            {
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> RemoveAsync(int id)
+        {
+            try
+            {
+                await _productCategoryRepository.RemoveAsync(id);
+            }
+            catch (NotFoundException)
+            {
+                throw;
+            }
+            
+            return NoContent();
         }
     }
 }
